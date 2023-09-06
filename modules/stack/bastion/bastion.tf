f000aa9e6f8c98c38b7793aa009a7d494c910ead
Rod Stewart <rodstewart@gmail.com>
2023-09-06 15:37:33 -0500

    Tests using TFC registry

--- modules/stack/bastion/bastion.tf
+++ modules/stack/bastion/bastion.tf
@@ -23,7 +23,8 @@ data "azurerm_subnet" "bastion" {
 
 module "bastion_host" {
   # source   = "github.com/rodmhgl/BastionHost?ref=v2.0.0"
-  source   = "rodmhgl/bastionhost/azurerm"
+  # source   = "rodmhgl/bastionhost/azurerm"
+  source   = "app.terraform.io/rnlabs/bastionhost/azurerm"
   version  = "2.2.0"
   for_each = local.regions
 
