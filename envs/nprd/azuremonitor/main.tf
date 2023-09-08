9a071957bd66ca57a1d425b223894f4b017ba211
Rod Stewart <rodstewart@gmail.com>
2023-09-07 21:57:06 -0500

    Adds envs output

--- envs/nprd/azuremonitor/main.tf
+++ envs/nprd/azuremonitor/main.tf
@@ -35,6 +35,26 @@ variable "tags" {
   description = "A map of tags to add to all resources."
 }
 
+output "azmonitor_out" {
+  value = module.AzureMonitor
+}
+
+output "law_out" {
+  value = module.AzureMonitor.law_out
+}
+
+output "sa_out" {
+  value = module.AzureMonitor.sa_out
+}
+
+output "ehnamespace_out" {
+  value = module.AzureMonitor.ehnamespace_out
+}
+
+output "eh_out" {
+  value = module.AzureMonitor.eh_out
+}
+
 module "AzureMonitor" {
   source      = "../../../modules/stack/azuremonitor"
   prefix      = var.prefix
